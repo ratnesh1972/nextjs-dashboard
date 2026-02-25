75ec7db1b4dae1381c6e54337795ae83d70c979f
Ratnesh Patil <ratneshdesigns@gmail.com>
2026-02-25 13:42:23 +0530

    add metadata dynamically

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: 'Customers',
+    description: 'View and manage your customers in the Next.js Dashboard.',
+};
+
 export default function Page() {
     return (
         <p>Customers Page</p>
